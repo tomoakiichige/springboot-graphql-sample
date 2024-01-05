b773eb6a1d1260647d510563809d09ee018eb098
Tomoaki Ichige <ichigetomoaki1207@gmail.com>
2024-01-06 05:46:05 +0900

    init

---
+++ src/main/kotlin/com/example/springbootgraphqlsample/SpringbootGraphqlSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootgraphqlsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootGraphqlSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootGraphqlSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootgraphqlsample/SpringbootGraphqlSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootgraphqlsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootGraphqlSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
