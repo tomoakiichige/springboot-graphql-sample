db7b02419c7b7e135142301fcf49b8c00c5a3a10
Tomoaki Ichige <ichigetomoaki1207@gmail.com>
2024-01-06 06:21:50 +0900

    implement

---
+++ src/main/kotlin/com/example/springbootgraphqlsample/Book.kt
@@ -0,0 +1,20 @@
+package com.example.springbootgraphqlsample
+
+data class Book(
+    val id: String,
+    val name: String,
+    val pageCount: Int,
+    val authorId: String
+) {
+    companion object {
+        private val books = mutableListOf(
+            Book("book-1", "Effective Java", 416, "author-1"),
+            Book("book-2", "Hitchhiker's Guide to the Galaxy", 208, "author-2"),
+            Book("book-3", "Down Under", 436, "author-3")
+        )
+
+        fun getById(id: String): Book? {
+            return books.firstOrNull { it.id == id }
+        }
+    }
+}
